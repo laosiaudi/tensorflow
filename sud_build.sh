1add62c3696f8a8c38dc002df46ddafd20f88f28
xiangguang zheng <xiangguz@s-3.tensorflow.cmu849.emulab.net>
2016-12-11 17:08:03 -0700

    delay test on s3

--- sud_build.sh
+++ sud_build.sh
@@ -1,4 +1,6 @@
+sudo rm -f /tmp/*.log
 bazel build -c opt //tensorflow/tools/pip_package:build_pip_package
 
 sudo bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 sudo -H pip install --force-reinstall /tmp/tensorflow_pkg/tensorflow-0.12.0rc0-cp27-none-linux_x86_64.whl
+sudo python tensorflow/examples/tutorials/mnist/mnist_with_summaries.py
