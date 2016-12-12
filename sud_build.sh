59abddf954ebeebc3fe2822f074ef0de0b732bdf
xiangguang zheng <xiangguz@s-2.tensorflow.cmu849.emulab.net>
2016-12-11 18:24:50 -0700

    test

--- sud_build.sh
+++ sud_build.sh
@@ -3,4 +3,4 @@ bazel build -c opt //tensorflow/tools/pip_package:build_pip_package
 
 sudo bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 sudo -H pip install --force-reinstall /tmp/tensorflow_pkg/tensorflow-0.12.0rc0-cp27-none-linux_x86_64.whl
-sudo python tensorflow/examples/tutorials/mnist/mnist_with_summaries.py
+#sudo python tensorflow/examples/tutorials/mnist/mnist_with_summaries.py
