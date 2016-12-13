75c4806112d14c35f61641e57bf988cbd4ec5ff5
root <root@s-2.tensorflow.cmu849.emulab.net>
2016-12-12 21:34:58 -0700

    fixed rand

--- tensorflow/core/common_runtime/executor.cc
+++ tensorflow/core/common_runtime/executor.cc
@@ -1464,7 +1464,7 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_usec) {
 
                 
           }
-	  auto r = ((double) rand() / (RAND_MAX))
+	  auto r = ((double) rand() / (RAND_MAX));
           delay = (int)(delay*0.6+(r)*0.1);
           if (delay < 5000)
 		delay = 0;
